50b84d80b547a95a5d5ad68e67e5a4502662afc3
Kris Jenkins <krisajenkins@gmail.com>
2016-08-30 05:15:53 +0100

    Add.

--- src/View.elm
+++ src/View.elm
@@ -7,6 +7,15 @@ import Types exposing (..)
 root : Model -> Html Msg
 root model =
     div []
-        [ code []
-            [ text (toString model) ]
-        ]
+        (case model.news of
+            Ok stories ->
+                List.map storyView stories
+
+            Err err ->
+                [ text (toString err) ]
+        )
+
+
+storyView : Story -> Html Msg
+storyView story =
+    h3 [] [ text story.title ]
